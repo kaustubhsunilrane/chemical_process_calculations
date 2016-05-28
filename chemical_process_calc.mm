ba148914a0e0c817ebb77e230c18163728b299f3
Kaustubhsunilrane <kaustubh.rane21@gmail.com>
2016-05-28 10:27:54 +0200

    Added the outline of the lectures

---
+++ chemical_process_calc.mm
@@ -0,0 +1,162 @@
+<map version="docear 1.1" project="154CA3DE0AA89XBMKBLWQNJV4NE8WRQCX2F4" project_last_home="file:/C:/Users/Kaustubh/Dropbox/research/projects/teaching/chemical_process_calc/" dcr_id="1463682540237_6hw5wu7hqedy6xbjiwv58lihv">
+<!--To view this file, download Docear - The Academic Literature Suite from http://www.docear.org -->
+<node TEXT="Chemical process calculations" FOLDED="false" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1463682557857" NUMBERED="true"><hook NAME="MapStyle" layout="OUTLINE">
+    <properties show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<node TEXT="Lectures (13 hrs)" POSITION="right" ID="ID_1273915956" CREATED="1463685591377" MODIFIED="1464252594820" NUMBERED="true">
+<edge COLOR="#0000ff"/>
+<node TEXT="Introduction" ID="ID_1297843199" CREATED="1464253710084" MODIFIED="1464253732306" NUMBERED="true">
+<node TEXT="Course_instructions" ID="ID_1268060459" CREATED="1464253788866" MODIFIED="1464253941833" MOVED="1464253927422" NUMBERED="true"/>
+<node TEXT="Chemical process" ID="ID_691304137" CREATED="1464253733653" MODIFIED="1464253740173" NUMBERED="true"/>
+<node TEXT="Common terms" ID="ID_1596225868" CREATED="1464254104603" MODIFIED="1464254114583" NUMBERED="true"/>
+<node TEXT="Assumptions" ID="ID_1154466711" CREATED="1464253742093" MODIFIED="1464253748643" NUMBERED="true"/>
+</node>
+<node TEXT="Introduction to material balance" ID="ID_1146283451" CREATED="1464255034713" MODIFIED="1464255051233" NUMBERED="true">
+<node TEXT="Information" ID="ID_521297917" CREATED="1464256009291" MODIFIED="1464256017680" MOVED="1464256020201" NUMBERED="true">
+<node TEXT="Explicit" ID="ID_1893794111" CREATED="1464256055680" MODIFIED="1464256081693" NUMBERED="true"/>
+<node TEXT="Implicit" ID="ID_1874079912" CREATED="1464256082216" MODIFIED="1464256085188" NUMBERED="true"/>
+</node>
+<node TEXT="Process types" ID="ID_95341715" CREATED="1464255158043" MODIFIED="1464255221912" MOVED="1464255563699" NUMBERED="true">
+<node TEXT="Stead-state processes" ID="ID_567790025" CREATED="1464351695250" MODIFIED="1464351703188" NUMBERED="true"/>
+<node TEXT="Transient processes" ID="ID_759335706" CREATED="1464351703762" MODIFIED="1464351710821" NUMBERED="true"/>
+</node>
+<node TEXT="Conservation of matter" ID="ID_1687908849" CREATED="1464255053783" MODIFIED="1464255276393" NUMBERED="true">
+<node TEXT="Non-reactive processes" ID="ID_1910130281" CREATED="1464255655686" MODIFIED="1464255665743" NUMBERED="true"/>
+<node TEXT="Reactive  processes" ID="ID_882137451" CREATED="1464255666349" MODIFIED="1464255676169" NUMBERED="true"/>
+</node>
+<node TEXT="Flow-charts and convention" ID="ID_670174417" CREATED="1464255222553" MODIFIED="1464255333737" NUMBERED="true"/>
+</node>
+<node TEXT="Getting Explicit information" ID="ID_438500704" CREATED="1464276606015" MODIFIED="1464277080433" MOVED="1464276609763" NUMBERED="true">
+<node TEXT="Literature sources" ID="ID_1433880034" CREATED="1464276669596" MODIFIED="1464276920177" NUMBERED="true"/>
+<node TEXT="Experimental measurements" ID="ID_1113646970" CREATED="1464276920840" MODIFIED="1464276928999" NUMBERED="true"/>
+</node>
+<node TEXT="Getting implicit information" ID="ID_1625463847" CREATED="1464277026959" MODIFIED="1464277098347" MOVED="1464277046250" NUMBERED="true">
+<node TEXT="Introduction to thermodynamic states" ID="ID_1099201293" CREATED="1464277100646" MODIFIED="1464277207145" NUMBERED="true"/>
+<node TEXT="Equations of states" ID="ID_76203198" CREATED="1464277207777" MODIFIED="1464277217906" NUMBERED="true"/>
+<node TEXT="Feasibility" ID="ID_1740856079" CREATED="1464277377942" MODIFIED="1464278231884" NUMBERED="true"/>
+</node>
+<node TEXT="Multi-phase processes 1" ID="ID_396763021" CREATED="1464255898379" MODIFIED="1464279183511" NUMBERED="true">
+<node TEXT="Examples of processes" ID="ID_829225125" CREATED="1464256781309" MODIFIED="1464279109462" NUMBERED="true"/>
+<node TEXT="Phase equilibria" ID="ID_1862098667" CREATED="1464256888257" MODIFIED="1464279170886" NUMBERED="true"/>
+<node TEXT="Liquid-vapor equilibria" ID="ID_163676329" CREATED="1464279191174" MODIFIED="1464279474886" NUMBERED="true">
+<node TEXT="Single component" ID="ID_1440796059" CREATED="1464279628358" MODIFIED="1464279637699" NUMBERED="true"/>
+<node TEXT="Multi-component" ID="ID_957144967" CREATED="1464279638223" MODIFIED="1464279645092" NUMBERED="true"/>
+</node>
+</node>
+<node TEXT="Multi-phase processes 2" ID="ID_1383717856" CREATED="1464279480041" MODIFIED="1464279491199" NUMBERED="true">
+<node TEXT="Solid-liquid equilibria" ID="ID_1452604097" CREATED="1464279494122" MODIFIED="1464422902057" NUMBERED="true">
+<node TEXT="Single component" ID="ID_564491061" CREATED="1464279650845" MODIFIED="1464422902071" NUMBERED="true"/>
+<node TEXT="Multi-component" ID="ID_1670029626" CREATED="1464279664579" MODIFIED="1464422902087" NUMBERED="true"/>
+</node>
+<node TEXT="Liquid-liquid equilibria" ID="ID_304310204" CREATED="1464279507531" MODIFIED="1464422902098" NUMBERED="true">
+<node TEXT="Single component" ID="ID_392342372" CREATED="1464279680397" MODIFIED="1464422902117" NUMBERED="true"/>
+<node TEXT="Multi-component" ID="ID_1923176069" CREATED="1464279694065" MODIFIED="1464422902130" NUMBERED="true"/>
+</node>
+</node>
+<node TEXT="Introduction to energy balance" ID="ID_909253388" CREATED="1464279577365" MODIFIED="1464280205377" NUMBERED="true">
+<node TEXT="First law of thermodynamics" ID="ID_1787574803" CREATED="1464280228999" MODIFIED="1464280249209" NUMBERED="true"/>
+<node TEXT="Forms of energy" ID="ID_684107809" CREATED="1464280261551" MODIFIED="1464281107369" NUMBERED="true"/>
+<node TEXT="Work and heat" ID="ID_405243603" CREATED="1464281108116" MODIFIED="1464281119556" NUMBERED="true"/>
+<node TEXT="Second law of thermodynamics" ID="ID_339707016" CREATED="1464281121704" MODIFIED="1464281140867" NUMBERED="true"/>
+<node TEXT="Interaction between energy and material" ID="ID_1727842212" CREATED="1464281295562" MODIFIED="1464281345809" NUMBERED="true">
+<node TEXT="Change in state" ID="ID_1776889481" CREATED="1464281346943" MODIFIED="1464281362367" NUMBERED="true"/>
+<node TEXT="Phase change" ID="ID_1705147363" CREATED="1464281365019" MODIFIED="1464281380519" NUMBERED="true"/>
+<node TEXT="Chemical reactions" ID="ID_387723986" CREATED="1464281380944" MODIFIED="1464281395604" NUMBERED="true"/>
+</node>
+<node TEXT="Examples of applications" ID="ID_1015332091" CREATED="1464282008717" MODIFIED="1464282019661" NUMBERED="true"/>
+</node>
+<node TEXT="Determination of energetic components" ID="ID_456807201" CREATED="1464281415352" MODIFIED="1464282886094" NUMBERED="true">
+<node TEXT="Common names of different components" ID="ID_894292127" CREATED="1464282790071" MODIFIED="1464283016916" MOVED="1464282848469" NUMBERED="true"/>
+<node TEXT="Determination using specific heats" ID="ID_1628878499" CREATED="1464282715600" MODIFIED="1464283071562" NUMBERED="true"/>
+<node TEXT="Determination using compiled data (Specific types of plots)" ID="ID_1836801738" CREATED="1464282764187" MODIFIED="1464283131047" NUMBERED="true"/>
+</node>
+<node TEXT="Reactive processes" ID="ID_498648217" CREATED="1464283202466" MODIFIED="1464349745591" NUMBERED="true">
+<node TEXT="Conventions for stoichiometry and reaction rates" ID="ID_1919883950" CREATED="1464348367990" MODIFIED="1464348904426" NUMBERED="true"/>
+<node TEXT="Heats of reaction" ID="ID_703976165" CREATED="1464349247651" MODIFIED="1464349381858" NUMBERED="true">
+<node TEXT="Heat of formation" ID="ID_1821401147" CREATED="1464349383599" MODIFIED="1464349437739" NUMBERED="true"/>
+<node TEXT="Heat of combustion" ID="ID_161915598" CREATED="1464349438281" MODIFIED="1464349445202" NUMBERED="true"/>
+<node TEXT="Heat of formation of solutions" ID="ID_1497468424" CREATED="1464349446057" MODIFIED="1464349476324" NUMBERED="true"/>
+</node>
+<node TEXT="Determination of heats of reaction" ID="ID_1979338689" CREATED="1464348905335" MODIFIED="1464348979237" NUMBERED="true">
+<node TEXT="Explicit" ID="ID_921289160" CREATED="1464348935184" MODIFIED="1464348966676" NUMBERED="true"/>
+<node TEXT="Implicit" ID="ID_312397051" CREATED="1464348956273" MODIFIED="1464348959940" NUMBERED="true"/>
+</node>
+</node>
+<node TEXT="Overview of special types of processes" ID="ID_753187293" CREATED="1464349582926" MODIFIED="1464351148939" NUMBERED="true">
+<node TEXT="Flames and ignitions" ID="ID_1810537505" CREATED="1464349605903" MODIFIED="1464349799039" NUMBERED="true"/>
+<node TEXT="Hazardous processes" ID="ID_567508890" CREATED="1464349800029" MODIFIED="1464350335096" NUMBERED="true"/>
+<node TEXT="Pharmaceutical processes" ID="ID_1419871685" CREATED="1464350243290" MODIFIED="1464350792357" NUMBERED="true">
+<node TEXT="Non-reactive" ID="ID_280970738" CREATED="1464351177633" MODIFIED="1464351186235" NUMBERED="true"/>
+<node TEXT="Reactive" ID="ID_1493950118" CREATED="1464351186874" MODIFIED="1464351190332" NUMBERED="true"/>
+</node>
+<node TEXT="Processes outside the chemical industry" ID="ID_537190033" CREATED="1464350916537" MODIFIED="1464351396193" NUMBERED="true"/>
+</node>
+<node TEXT="Process simulation" ID="ID_98584891" CREATED="1464352183298" MODIFIED="1464356685046" NUMBERED="true">
+<node TEXT="Flow of information" ID="ID_303392147" CREATED="1464357205013" MODIFIED="1464357291458" NUMBERED="true"/>
+<node TEXT="Sequential modeular simulation" ID="ID_1204580038" CREATED="1464357291952" MODIFIED="1464357316147" NUMBERED="true"/>
+<node TEXT="Equation-based simulation" ID="ID_1658790362" CREATED="1464357316433" MODIFIED="1464357340836" NUMBERED="true"/>
+<node TEXT="Outlook" ID="ID_451676462" CREATED="1464357341794" MODIFIED="1464360226081" NUMBERED="true"/>
+</node>
+</node>
+<node TEXT="Tutorials" POSITION="right" ID="ID_1941821283" CREATED="1463685606489" MODIFIED="1463685610622" NUMBERED="true">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Assignments" POSITION="right" ID="ID_36194819" CREATED="1463685611099" MODIFIED="1463685616719" NUMBERED="true">
+<edge COLOR="#ff00ff"/>
+</node>
+<node TEXT="Projects" POSITION="right" ID="ID_1087949929" CREATED="1463686134743" MODIFIED="1463686156452" NUMBERED="true">
+<edge COLOR="#ffff00"/>
+</node>
+<node TEXT="Exams" POSITION="right" ID="ID_209760937" CREATED="1463686148611" MODIFIED="1463686169672" NUMBERED="true">
+<edge COLOR="#7c0000"/>
+</node>
+</node>
+</map>
